3743cbabce658dbf5df99163ab59e1b30028e1f5
mk-050 <yoshikawa.0505@outlook.jp>
2024-11-24 22:08:05 +0900

    regist_confirm.phpの編集

--- regist_confirm.php
+++ regist_confirm.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>account</title>
-    <link rel="stylesheet" type="text/css" href="regist.css" </head>
+    <link rel="stylesheet" type="text/css" href="regist2.css" </head>
 
 <body>
     <header>
@@ -26,59 +26,65 @@
         <div class="main-container">
 
             <form>
-                <div><label>名前（姓）</label>
-                    
-                </div><br>
+                <div class="account">
 
-                <div><label>名前（名）</label>
-                    
-                </div><br>
+                    <div><label>名前（姓）</label>
 
-                <div><label>カナ（姓）</label>
-                    
-                </div><br>
+                    </div><br>
 
-                <div><label>カナ（名）</label>
-                    
-                </div><br>
+                    <div><label>名前（名）</label>
 
-                <div><label>メールアドレス</label>
-                    
-                </div><br>
+                    </div><br>
 
-                <div><label>パスワード</label>
-                    
-                </div><br>
+                    <div><label>カナ（姓）</label>
 
-                <div><label>性別</label>
-                    
-                </div><br>
+                    </div><br>
 
-                <div><label>郵便番号</label>
-                    
-                </div><br>
+                    <div><label>カナ（名）</label>
 
-                <div><label>住所（都道府県）</label>
+                    </div><br>
 
-                </div><br>
+                    <div><label>メールアドレス</label>
 
-                <div><label>住所（市区町村）</label>
-                   
-                </div><br>
+                    </div><br>
 
-                <div><label>住所（番地）</label>
-                    
-                </div><br>
+                    <div><label>パスワード</label>
 
-                <div><label>アカウント権限</label>
-                   
-                </div><br>
+                    </div><br>
 
-                <span><input type="submit" class="submit" value="前に戻る" ></span>
-                <span><input type="submit" class="submit" value="登録する" ></span>
+                    <div><label>性別</label>
+
+                    </div><br>
+
+                    <div><label>郵便番号</label>
+
+                    </div><br>
+
+                    <div><label>住所（都道府県）</label>
+
+                    </div><br>
+
+                    <div><label>住所（市区町村）</label>
+
+                    </div><br>
+
+                    <div><label>住所（番地）</label>
+
+                    </div><br>
+
+                    <div><label>アカウント権限</label>
+
+                    </div><br>
+                </div>
+
+                <input type="submit" class="submit" value="前に戻る">
+                <input type="submit" class="submit" value="登録する">
 
             </form>
         </div>
+
+
+
 
     </main>
 
