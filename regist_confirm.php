0a7d7060b2d3a3dc65047760db3ee257ca420f5a
mk-050 <yoshikawa.0505@outlook.jp>
2024-11-24 20:46:57 +0900

    regist_confirm.phpの作成と編集

---
+++ regist_confirm.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>account</title>
+    <link rel="stylesheet" type="text/css" href="regist.css" </head>
+
+<body>
+    <header>
+        <ul>
+            <li>トップ</li>
+            <li>プロフィール</li>
+            <li>D.I.Blogについて</li>
+            <li>登録フォーム</li>
+            <li>問い合わせ</li>
+            <li>その他</li>
+            <li>アカウント登録</li>
+        </ul>
+
+    </header>
+
+    <h1>アカウント登録確認画面</h1><br>
+
+    <main>
+        <div class="main-container">
+
+            <form>
+                <div><label>名前（姓）</label>
+                    
+                </div><br>
+
+                <div><label>名前（名）</label>
+                    
+                </div><br>
+
+                <div><label>カナ（姓）</label>
+                    
+                </div><br>
+
+                <div><label>カナ（名）</label>
+                    
+                </div><br>
+
+                <div><label>メールアドレス</label>
+                    
+                </div><br>
+
+                <div><label>パスワード</label>
+                    
+                </div><br>
+
+                <div><label>性別</label>
+                    
+                </div><br>
+
+                <div><label>郵便番号</label>
+                    
+                </div><br>
+
+                <div><label>住所（都道府県）</label>
+
+                </div><br>
+
+                <div><label>住所（市区町村）</label>
+                   
+                </div><br>
+
+                <div><label>住所（番地）</label>
+                    
+                </div><br>
+
+                <div><label>アカウント権限</label>
+                   
+                </div><br>
+
+                <span><input type="submit" class="submit" value="前に戻る" ></span>
+                <span><input type="submit" class="submit" value="登録する" ></span>
+
+            </form>
+        </div>
+
+    </main>
+
+    <footer>
+        Copyright D.I.works| D.I.blog is the one which provides A to Z about programming
+    </footer>
+
+</body>
+
+</html>
